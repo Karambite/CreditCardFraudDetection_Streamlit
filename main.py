69b1c11f861d76ffc430893c706c5e7eaba93a21
Sudesh Easwaran <70345588+Karambite@users.noreply.github.com>
2022-12-03 15:32:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import  LabelEncoder
-import xgboost as xgb
-import numpy as np
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import  LabelEncoder
+#import xgboost as xgb
+#import numpy as np
 st.header("Credit Card Fraud Detection")
 
 st.subheader("Please select relevant features of your fish!")
