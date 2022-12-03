cde94f30687e84787036b247ead11e39a9a905d4
Sudesh Easwaran <70345588+Karambite@users.noreply.github.com>
2022-12-03 15:48:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
 import xgboost as xgb
+from tensorflow.keras.models import load_model
+tf_model = load_model('tf_model.json')
+rfc_model = pickle.load(open('RandomForestClassifier_model.json','rb'))
+
 st.header("Credit Card Fraud Detection")
 
 st.subheader("Enter an input array")
