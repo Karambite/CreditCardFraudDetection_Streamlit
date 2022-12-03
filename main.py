d403bb7799ef977bd6b71f19868bcb618e9e2951
Sudesh Easwaran <70345588+Karambite@users.noreply.github.com>
2022-12-03 15:31:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,10 @@ from sklearn.preprocessing import  LabelEncoder
 import xgboost as xgb
 import numpy as np
 st.header("Credit Card Fraud Detection")
-data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/Fish.csv")
-#load label encoder
-encoder = LabelEncoder()
-encoder.classes_ = np.load('classes.npy',allow_pickle=True)
-
-# load model
-best_xgboost_model = xgb.XGBRegressor()
-best_xgboost_model.load_model("best_model.json")
 
 st.subheader("Please select relevant features of your fish!")
-st.text_input("Enter your input array: ", key="name")
+input = st.text_input("Enter your input array: ", key="name")
 
 
 if st.button('Make Prediction'):
-    input_species = encoder.transform(np.expand_dims(inp_species, -1))
-    inputs = np.expand_dims(
-        [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
-    prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
-
-    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-    st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
+    st.write(input)
